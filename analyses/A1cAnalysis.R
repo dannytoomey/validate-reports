7f7e73177c9f159d698f3cb605458788a8d5238f
dannytoomey <dtoomey@umail.ucsb.edu>
2023-06-08 17:10:03 -0700

    add a1c thresholds and download button

--- analyses/A1cAnalysis.R
+++ analyses/A1cAnalysis.R
@@ -6,14 +6,24 @@
 #' @param A1cThreshold  A cutoff value to exclude patients from analysis.
 #' 						Patients whose first A1c within the given range is below this
 #' 						value will not be included in analysis. 
+#' @param A1cFinalValue A final value to define a significant change.
 #' @return 				A data frame with A1c change results. Idenitfying patient information 
 #'						is not analysed or returned. 
 
-a1c_analysis <- function(dataframe,type,A1cThreshold){
+a1c_analysis <- function(dataframe,type,A1cThreshold,A1cFinalValue){
+
+	A1cThreshold <- as.numeric(A1cThreshold)
+	if(is.na(A1cThreshold)){
+		stop("Please input a value for A1c threshold.")
+	}
+	A1cFinalValue <- as.numeric(A1cFinalValue)
+	if(is.na(A1cFinalValue)){
+		stop("Please input a value for final A1c value.")
+	}
 	
 	chartNums = unique(dataframe$`Chart.`)
-	A1cReport <- data.frame(matrix(ncol = 9, nrow = 0))
-	colnames(A1cReport) <- c('Chart_num','Birth_Year','Race','Ethnicity','First_Result_Date','First_Result_Value','Last_Result_Date','Last_Result_Value','Change')
+	A1cReport <- data.frame(matrix(ncol = 10, nrow = 0))
+	colnames(A1cReport) <- c('Chart_num','Birth_Year','Race','Ethnicity','First_Result_Date','First_Result_Value','Last_Result_Date','Last_Result_Value','Change','Final_result_below_threshold')
 
 	for(chart in chartNums){
 		patient = dataframe[dataframe$`Chart.`==chart,]
@@ -51,6 +61,12 @@ a1c_analysis <- function(dataframe,type,A1cThreshold){
 					if(first_result_value==last_result_value){
 						change <- "No change"
 					}
+					if(last_result_value<A1cFinalValue){
+						final_result_below_threshold <- "Yes"
+					}
+					if(last_result_value>=A1cFinalValue){
+						final_result_below_threshold <- "No"
+					}
 					
 					if(type=="csv"){
 						entry <- c(array[nrow(array),]$Chart_num,
@@ -74,13 +90,13 @@ a1c_analysis <- function(dataframe,type,A1cThreshold){
 								   first_result_value,
 								   last_result_date,
 								   last_result_value,
-								   change
+								   change,
+								   final_result_below_threshold
 								  )
 					}
 					
-
 					A1cReport[nrow(A1cReport)+1,] <- entry
-					
+										
 				}	
 			}	
 		}
